efc04c2529bfa34027cead4989b5d5fdd5a0165c
Dominier <118577440+Dominier@users.noreply.github.com>
2023-06-25 03:37:37 -0700

    Added plus button 28:53

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,45 @@
 "use client"
 
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai"
+
 const Library = () => {
     const onClick = () => {
         // handle upload later
     };
     return(
-        <div>
-            Library!
+        <div className="flex flex-col">
+            <div className="
+                flex
+                items-center
+                justify-between
+                px-5
+                pt-4
+            ">
+                <div className="
+                    inline-flex
+                    items-center
+                    gap-x-2
+                ">
+                    <TbPlaylist className="text-neutral-400" size={26}/>
+                    <p className="
+                        text-neutral-400
+                        font-medium
+                        text-md
+                    ">
+                        Your Library
+                    </p>
+                </div>
+                <AiOutlinePlus
+                    onClick={onClick}
+                    size={20}
+                    className="
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    "/>
+            </div>
         </div>
     );
 }
