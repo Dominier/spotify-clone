c33b2f5c8bfe02d91ed1624440d85cb8d2383e56
Dominier <118577440+Dominier@users.noreply.github.com>
2023-06-26 02:08:12 -0700

    1:27:55

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 import { Database } from "@/types_db";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 interface SupabaseProviderProps {
     children: React.ReactNode;
@@ -14,5 +15,11 @@ const SupabaseProviderProps: React.FC<SupabaseProviderProps> = ({
 }) => {
     const [supabaseCilent] = useState(() =>
         createClientComponentClient<Database>()
+    );
+
+    return (
+       <SessionContextProvider supabaseClient={supabaseCilent}>
+            {children}
+       </SessionContextProvider>
     )
 }
