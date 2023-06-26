9bd0e211341de5f810810fa5e30a979500ec68a8
Dominier <118577440+Dominier@users.noreply.github.com>
2023-06-26 01:59:29 -0700

    created supabaseprovider

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Database } from "@/types_db";
+
+interface SupaBaseProviderProps {
+    children: React.ReactNode;
+};
+
+const SupaBaseProviderProps: React.FC<SupabaseProviderProps> = ({
+    children
+}) => {
+    
+}
