a0f676de0ba6291681aca8457d707decc520dcfc
Dominier <118577440+Dominier@users.noreply.github.com>
2023-06-26 23:40:34 -0700

    finish hook

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,5 +1,5 @@
 import { User } from "@supabase/auth-helpers-nextjs";
-import { useEffect, createContext, useState } from "react";
+import { useEffect, createContext, useState, useContext } from "react";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
 
 import { Subscription, UserDetails } from "@/types";
@@ -75,4 +75,12 @@ export const MyUserContextProvider = (props: Props) => {
     };
 
     return <UserContext.Provider value={value} {...props}/>;
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a MyUserContextProvider');
+    }
+    return context;
 };
