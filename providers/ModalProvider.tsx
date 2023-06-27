3f030df07039e0e83275d9ae6db890547c055709
Dominier <118577440+Dominier@users.noreply.github.com>
2023-06-26 23:58:15 -0700

    created ModalProviders

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    return (
+        <>
+            Modals
+        </>
+    )
+}
+
+export default ModalProvider;
