b2f5871dcbabc05134be3a4a4afe5444ffa31890
Dominier <118577440+Dominier@users.noreply.github.com>
2023-06-30 00:38:36 -0700

    finish input.tsx? 2:40:39

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,17 +1,44 @@
 "use client"
 
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+
 import useUploadModal from "@/hooks/useUploadModal";
+
 import Modal from "./Modal";
+import Input from "./Input";
 
 const UploadModal = () => {
+    const [isLoading, setIsLoading] = useState();
     const uploadModal = useUploadModal();
+
+    const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm<FieldValues>({
+        defaultValues: {
+            author: '',
+            title: '',
+            song: null,
+            image: null,
+        }
+    })
+
+    // Everytime user closes form, it will reset the form
     const onChange = (open: boolean) => {
         if (!open) {
-            // reset the form
+            reset();
             uploadModal.onClose()
         }
-
     }
+
+
+    // access to author, title, song, and image upon submit
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+        // Upload to supabase
+    }
+
     return (
         <Modal
             title="Add a song"
@@ -19,7 +46,16 @@ const UploadModal = () => {
             isOpen={uploadModal.isOpen}
             onChange={onChange}
         >
-            Form
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <Input 
+                    id="title"
+                    disabled={isLoading}   
+                    {...register('title', { required: true })} // spread props and attributes that we will need for input
+                    placeholder="Song title"
+                />
+            </form>
         </Modal>
     );
 }
