20ab30c294cc1dac9ad3d51f7d7038b825ab5398
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-05 00:02:27 -0700

    className update 3:08:21

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import Image from "next/image";
 
 interface SongItemProps {
     data: Song;
@@ -15,8 +16,33 @@ const SongItem:React.FC<SongItemProps> = ({
 const imagePath = useLoadImage(data);
 
     return (
-        <div>
-            Song Item
+        <div
+            onClick={() => onClick(data.id)}
+            className="
+                relative
+                group
+                flex
+                flex-col
+                items-center
+                justify-center
+                rounded-md
+                overflow-hidden
+                gap-x-4
+                bg-neutral-400/5
+                cursor-pointer
+                hover:bg-neutral-400/10
+                transition
+                p-3
+            "
+        >
+            <div
+                className="
+                    relative
+                ">
+                <Image 
+
+                />
+            </div>
         </div>
     );
 }
