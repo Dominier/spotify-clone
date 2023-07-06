9cc3828f50f6509c9b19d63c33870a0f10ae75dd
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-06 01:04:56 -0700

    create mediaitem, added mediaitem to sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from '@/providers/UserProvider'
 import Sidebar from '@/components/Sidebar'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserID'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -15,11 +16,14 @@ export const metadata = {
   description: 'Listening to music',
 }
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
       <body className={font.className}>
@@ -27,7 +31,7 @@ export default function RootLayout({
           <SupabaseProvider>
             <UserProvider>
               <ModalProvider />
-              <Sidebar>
+              <Sidebar songs={ userSongs }>
                 {children}
               </Sidebar>
             </UserProvider>
---
+++ components/MediaItem.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import useLoadImage from "@/hooks/useLoadImage";
+
+interface MediaItemProps {
+    data: Song;
+    onClick?: (id: string) => void;
+}
+
+const MediaItem: React.FC<MediaItemProps> = ({
+    data,
+    onClick
+}) => {
+    const imageUrl = useLoadImage(data);
+
+    const handleClick = () => {
+        if (onClick) {
+            return onClick(data.id);
+        }
+
+        // TODO: default turn on player
+    }
+
+    return (
+        <div>
+            Media Item
+        </div>
+    );
+}
+
+export default MediaItem;
