4b0000749383b573229b050e2ccf3b1a64759c33
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-07 20:00:06 -0700

    EVERYTHING WORKS NOW?

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -33,7 +33,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
                 items-center
                 gap-x-3
                 cursor-pointer
-                hover:bg-neutral-800/50
+                hover:bg-neutral-600/50
                 w-full
                 p-2
                 rounded-md
