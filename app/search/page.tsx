8aa435d84f1daf280841b8bd7e30ad4c3eff47b1
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-07 19:05:03 -0700

    ERROR Search. Finish SearchContent 3:41:19

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
 
 interface SearchProps {
     searchParams: {
@@ -30,6 +31,7 @@ const Search = async ({ searchParams }: SearchProps) => {
                     <SearchInput />
                 </div>
             </Header>
+            <SearchContent songs={songs} />
         </div>
     )
 };
