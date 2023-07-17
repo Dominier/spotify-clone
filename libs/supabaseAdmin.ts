18e9d1d8a53ddec49a19eb3fab7e9d1cef875b0e
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-16 23:29:50 -0700

    Finish createOrRetrieveCustomer 5:23:44

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -71,4 +71,28 @@ const createOrRetrieveCustomer = async ({
         .select('stripe_customer_id')
         .eq('id', uuid)
         .single();
-}
+
+    if (error || !data?.stripe_customer_id) {
+        const customerData: { metadata: {supabaseUUID: string }; email?: string } = {
+            metadata: {
+                supabaseUUID: uuid
+            }
+        };
+
+        if (email) customerData.email = email
+
+        const customer = await stripe.customers.create(customerData);
+        const { error: supabaseError } = await supabaseAdmin
+            .from('customers')
+            .insert([{ id: uuid, stripe_customer_id: customer.id }]);
+
+        if (supabaseError) {
+            throw supabaseError;
+        }
+
+        console.log(`New customer created and inserted for ${uuid}`);
+        return customer.id;
+    };
+    
+    return data.stripe_customer_id;
+};
