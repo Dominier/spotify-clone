c8c1d0b573f1dce0c3ad1122028aeb0a7c89e6bd
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-16 23:43:16 -0700

    5:26:18

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -96,3 +96,25 @@ const createOrRetrieveCustomer = async ({
     
     return data.stripe_customer_id;
 };
+
+// copy billing details to customer
+const copyBillingDetailsToCustomer = async (
+    uuid: string,
+    payment_method: Stripe.PaymentMethod
+) => {
+    const customer = payment_method.customer as string;
+    const { name, phone, address } = payment_method.billing_details;
+    if (!name || !phone || !address ) return;
+    //@ts-ignore
+    await stripe.customers.update(customer, { name, phone, address });
+    const { error } = await supabaseAdmin
+        .from('users')
+        .update({
+            billing_adress: { ...address },
+            payment_method: { ...payment_method[payment_method.type] }
+        })
+        .eq('id', uuid);
+        if (error) throw error;
+};
+
+// manage subscription
