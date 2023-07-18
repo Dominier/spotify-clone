f77cf222f176d16dabd48ddf267cab1b4553334f
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-17 23:00:03 -0700

    POST SET and ERR 5:42:51

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,11 +1,40 @@
-import Stripe from "stripe";
-import { NextResponse } from "next/server";
-import { headers } from "next/headers";
+import Stripe from 'stripe';
+import { NextResponse } from 'next/server';
+import { headers } from 'next/headers';
 
-import { stripe } from "@/libs/stripe";
+import { stripe } from '@/libs/stripe';
 import {
-    upsertProductRecord,
-    upsertPriceRecord,
-    manageSubscriptionStatusChange
-} from "@/libs/supabaseAdmin"
+  upsertProductRecord,
+  upsertPriceRecord,
+  manageSubscriptionStatusChange
+} from '@/libs/supabaseAdmin';
 
+const relevantEvents = new Set([
+  'product.created',
+  'product.updated',
+  'price.created',
+  'price.updated',
+  'checkout.session.completed',
+  'customer.subscription.created',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
+]);
+
+export async function POST(
+    request: Request
+) {
+    const body = await request.text();
+    const sig = headers().get('Stripe-Signature');
+
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+    let event: Stripe.Event;
+
+    try {
+        if (!sig || !webhookSecret) return;
+        event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    }  catch (err: any) {
+        console.log('Error Message: ' + err.message);
+        return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 })
+    }
+
+}
