89edf46785481851f0edd6090ee4e162ece9747e
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-18 23:51:36 -0700

    relevantEvents 5:45:56

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -9,6 +9,7 @@ import {
   manageSubscriptionStatusChange
 } from '@/libs/supabaseAdmin';
 
+// event types
 const relevantEvents = new Set([
   'product.created',
   'product.updated',
@@ -34,7 +35,33 @@ export async function POST(
         event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
     }  catch (err: any) {
         console.log('Error Message: ' + err.message);
-        return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 })
+        return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
     }
 
+    // if revelantEvents has the event type (look above for types) we're looking for
+    if (relevantEvents.has(event.type)) {
+      try {
+        switch (event.type) {
+          case 'product.created':
+          case 'product.updated':
+            await upsertProductRecord(event.data.object as Stripe.Product);
+            break;
+          case 'price.created':
+          case 'price.updated':
+            await upsertPriceRecord(event.data.object as Stripe.Price);
+            break;
+          case 'customer.subscription.created':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+            await manageSubscriptionStatusChange(
+              subscription.id,
+              subscription.customer as string,
+              event.type === 'customer.subscription.created' // activates the createAction
+            );
+            break;
+            
+        }
+      }
+    }
 }
