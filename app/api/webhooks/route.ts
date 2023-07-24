3250e4c3549107441bad3d8b5ced9129ca57e8f0
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-23 20:29:42 -0700

    after 8 hours of debugging, price and product work

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -9,7 +9,6 @@ import {
   manageSubscriptionStatusChange
 } from '@/libs/supabaseAdmin';
 
-// event types
 const relevantEvents = new Set([
   'product.created',
   'product.updated',
@@ -22,63 +21,65 @@ const relevantEvents = new Set([
 ]);
 
 export async function POST(
-    request: Request
+  request: Request
 ) {
-    const body = await request.text();
+    const body = await request.text()
     const sig = headers().get('Stripe-Signature');
 
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+    const webhookSecret =
+      process.env.STRIPE_WEBHOOK_SECRET_LIVE ??
+      process.env.STRIPE_WEBHOOK_SECRET;
     let event: Stripe.Event;
 
     try {
-        if (!sig || !webhookSecret) return;
-        event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
-    }  catch (err: any) {
-        console.log('Error Message: ' + err.message);
-        return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+      if (!sig || !webhookSecret) return;
+      event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    } catch (err: any) {
+      console.log(`❌ Error message: ${err.message}`);
+      return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
     }
 
-    // if revelantEvents has the event type (look above for types) we're looking for
-    if (relevantEvents.has(event.type)) {
-      try {
-        switch (event.type) {
-          case 'product.created':
-          case 'product.updated':
-            await upsertProductRecord(event.data.object as Stripe.Product);
-            break;
-          case 'price.created':
-          case 'price.updated':
-            await upsertPriceRecord(event.data.object as Stripe.Price);
-            break;
-          case 'customer.subscription.created':
-          case 'customer.subscription.updated':
-          case 'customer.subscription.deleted':
-            const subscription = event.data.object as Stripe.Subscription;
+  if (relevantEvents.has(event.type)) {
+    try {
+      switch (event.type) {
+        case 'product.created':
+        case 'product.updated':
+          await upsertProductRecord(event.data.object as Stripe.Product);
+          break;
+        case 'price.created':
+        case 'price.updated':
+          await upsertPriceRecord(event.data.object as Stripe.Price);
+          break;
+        case 'customer.subscription.created':
+        case 'customer.subscription.updated':
+        case 'customer.subscription.deleted':
+          const subscription = event.data.object as Stripe.Subscription;
+          await manageSubscriptionStatusChange(
+            subscription.id,
+            subscription.customer as string,
+            event.type === 'customer.subscription.created'
+          );
+          break;
+        case 'checkout.session.completed':
+          const checkoutSession = event.data
+            .object as Stripe.Checkout.Session;
+          if (checkoutSession.mode === 'subscription') {
+            const subscriptionId = checkoutSession.subscription;
             await manageSubscriptionStatusChange(
-              subscription.id,
-              subscription.customer as string,
-              event.type === 'customer.subscription.created' // activates the createAction
+              subscriptionId as string,
+              checkoutSession.customer as string,
+              true
             );
-            break;
-          case 'checkout.session.completed':
-              const checkoutSession = event.data.object as Stripe.Checkout.Session;
-              if (checkoutSession.mode === 'subscription') {
-                const subscriptionId = checkoutSession.subscription;
-                await manageSubscriptionStatusChange(
-                  subscriptionId as string,
-                  checkoutSession.customer as string,
-                  true
-                );
-              }
-              break;
-            default:
-              throw new Error('Unhandled relevant event');
-        }
-      } catch (error) {
-        console.log(error);
-        return new NextResponse('Webhook error: "Webhook handler failed. View logs."', { status: 400 });
+          }
+          break;
+        default:
+          throw new Error('Unhandled relevant event!');
       }
+    } catch (error) {
+      console.log(error);
+      return new NextResponse('Webhook error: "Webhook handler failed. View logs."', { status: 400 });
     }
+  }
 
-    return NextResponse.json({ recieved: true }, {status: 200});
+  return NextResponse.json({ received: true }, { status: 200 });
 };
