568434dae25db89f5d7db909d5d9ea401a41e310
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-23 22:27:08 -0700

    SubscribeModal Finish

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,115 +1,124 @@
 "use client";
-import { toast } from "react-hot-toast";
-import { useState } from "react";
 
-import { Price, ProductWithPrice } from "@/types";
-import { useUser } from "@/hooks/useUser";
-import { postData } from "@/libs/helpers";
-import { getStripe } from "@/libs/stripeClient";
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
-import Modal from "./Modal";
-import Button from "./Button";
+import useSubscribeModal from '@/hooks/useSubscribeModal';
+import { useUser } from '@/hooks/useUser';
+import { postData } from '@/libs/helpers';
+import { getStripe } from '@/libs/stripeClient';
+import { Price, ProductWithPrice } from '@/types';
 
+import Modal from './Modal';
+import Button from './Button';
 
 interface SubscribeModalProps {
-    products: ProductWithPrice[];
+  products: ProductWithPrice[];
 }
 
 const formatPrice = (price: Price) => {
-    const priceString = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: price.currency,
-        minimumFractionDigits: 0
-    }).format((price?.unit_amount || 0) / 100);
+  const priceString = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: price.currency,
+    minimumFractionDigits: 0
+  }).format((price?.unit_amount || 0) / 100);
 
-    return priceString;
-}
+  return priceString;
+};
+
 
 const SubscribeModal: React.FC<SubscribeModalProps> = ({
-    products
+  products
 }) => {
-    const { user, isLoading, subscription } = useUser();
-    const [priceIdLoading, setPriceIdLoading] = useState<string>();
-    
-    const handleCheckout = async (price: Price) => {
-        setPriceIdLoading(price.id);
-        
-        if (!user) {
-            setPriceIdLoading(undefined);
-            return toast.error('Must be logged in');
-        }
+  const subscribeModal = useSubscribeModal();
+  const { user, isLoading, subscription } = useUser();
 
-        if (subscription) {
-            setPriceIdLoading(undefined);
-            return toast('Already subscribed');
-        }
+  const [priceIdLoading, setPriceIdLoading] = useState<string>();
 
-        try {
-            const { sessionId } = await postData({
-                url: '/api/create-checkout-session',
-                data: { price }
-            });
+  const onChange = (open: boolean) => {
+    if (!open) {
+      subscribeModal.onClose();
+    }
+  }
 
-            const stripe = await getStripe();
-            stripe?.redirectToCheckout({ sessionId });
-        } catch (error) {
-            toast.error((error as Error)?.message);
-        } finally {
-            setPriceIdLoading(undefined);
-        }
-    };
-    let content = (
-        <div className="text-center">
-            No products available.
-        </div>
-    );
-
-    if (products.length) {
-        content = (
-            <div>
-                {products.map((product) => {
-                    if (!product.prices?.length) {
-                        return (
-                            <div key={product.id}>
-                                No prices available
-                            </div>
-                        )
-                    }
-
-                    return product.prices.map((price) => (
-                        <Button 
-                            key={price.id}
-                            onClick={() => handleCheckout(price)}
-                            disabled={isLoading || price.id === priceIdLoading}
-                            className="mb-4"
-                            >
-                            {`Subscribe for ${formatPrice(price)} per ${price.interval}`}
-                        </Button>
-                    ))
-                })}
-            </div>
-        )
+  const handleCheckout = async (price: Price) => {
+    setPriceIdLoading(price.id);
+    if (!user) {
+      setPriceIdLoading(undefined);
+      return toast.error('Must be logged in');
     }
 
     if (subscription) {
-        content = (
-            <div className="text-center">
-                Already subscribed
-            </div>
-        )
+      setPriceIdLoading(undefined);
+      return toast('Already subscribed');
     }
 
+    try {
+      const { sessionId } = await postData({
+        url: '/api/create-checkout-session',
+        data: { price }
+      });
 
-    return (
-        <Modal
-            title="Only for premium users"
-            description="Listen to music with Spotify Premium"
-            isOpen
-            onChange={() => {}}
-        >
-            {content}
-        </Modal>
-    );
+      const stripe = await getStripe();
+      stripe?.redirectToCheckout({ sessionId });
+    } catch (error) {
+      return toast.error((error as Error)?.message);
+    } finally {
+      setPriceIdLoading(undefined);
+    }
+  };
+
+  let content = (
+    <div className="text-center">
+      No products available.
+    </div>
+  )
+
+  if (products.length) {
+    content = (
+      <div>
+        {products.map((product) => {
+          if (!product.prices?.length) {
+            return (
+              <div key={product.id}>
+                No prices available
+              </div>
+            );
+          }
+
+          return product.prices.map((price) => (
+            <Button 
+              key={price.id} 
+              onClick={() => handleCheckout(price)}
+              disabled={isLoading || price.id === priceIdLoading}
+              className="mb-4"
+            >
+              {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
+            </Button>
+          ))
+        })}
+      </div>
+    )
+  }
+
+  if (subscription) {
+    content = (
+      <div className="text-center">
+        Already subscribed.
+      </div>
+    )
+  }
+
+  return (
+    <Modal
+      title="Only for premium users"
+      description="Listen to music with Spotify Premium"
+      isOpen={subscribeModal.isOpen}
+      onChange={onChange}
+    >
+      {content}
+    </Modal>
+  );
 }
 
 export default SubscribeModal;
