7b9e799b26a6b645ca73e905cb38046963fc4f79
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-23 23:50:50 -0700

    Finish: Error with subscription

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Button from "@/components/Button";
 import useSubscribeModal from "@/hooks/useSubscribeModal";
 import { useUser } from "@/hooks/useUser";
 import { postData } from "@/libs/helpers";
@@ -33,11 +34,36 @@ const AccountContent = () => {
                 toast.error((error as Error).message);
             }
         }
+        setLoading(false);
     }
 
     return (
-        <div>
-            Account Content
+        <div className="mb-7 px-6">
+            {!subscription && (
+                <div className="flex flex-col gap-y-4">
+                    <p>No active plan.</p>
+                    <Button
+                        onClick={subscribeModal.onOpen}
+                        className="w-[300px]"
+                    >
+                        Subscribe
+                    </Button>
+                </div>
+            )}
+            {subscription && (
+                <div className="flex flex-col gap-y-4">
+                    <p>
+                        You are currently on the <b>{subscription?.price?.products?.name}</b> plan.
+                    </p>
+                    <Button
+                        disabled={loading || isLoading}
+                        onClick={redirectToCustomerPortal}
+                        className="w-[300px]"
+                    >
+                        Open customer portal
+                    </Button>
+                </div>
+            )}
         </div>
     );
 }
