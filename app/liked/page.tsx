1e21f37744804b0092d7bede77a64c89c3359d0f
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-26 23:05:53 -0700

    revalidate fix

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import Image from "next/image";
 import LikedContent from "./components/LikedContent";
 
-export const revaldiate = 0;
+export const revalidate = 0;
 
 const Liked = async () => {
     const songs = await getLikedSongs();
