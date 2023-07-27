d91c88509f181e77cd71b715c8d23159b1a0ae8b
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-26 21:18:32 -0700

    Update create-portal-link 6:00:00

--- app/api/create-portal-link/route.ts
+++ app/api/create-portal-link/route.ts
@@ -7,30 +7,30 @@ import { getURL } from '@/libs/helpers';
 import { createOrRetrieveCustomer } from '@/libs/supabaseAdmin';
 
 export async function POST() {
-    try {
-        const supabase = createRouteHandlerClient({
-            cookies
-        });
+  try {
+    const supabase = createRouteHandlerClient({ 
+      cookies
+     });
+    
+    const {
+      data: { user }
+    } = await supabase.auth.getUser();
 
-        const {
-            data: { user }
-        } = await supabase.auth.getUser();
+    if (!user) throw Error('Could not get user');
+    const customer = await createOrRetrieveCustomer({
+      uuid: user.id || '',
+      email: user.email || ''
+    });
 
-        if (!user) throw Error('Could not get user');
-        const customer = await createOrRetrieveCustomer({
-            uuid: user.id || '',
-            email: user.email || ''
-        });
+    if (!customer) throw Error('Could not get customer');
+    const { url } = await stripe.billingPortal.sessions.create({
+      customer,
+      return_url: `${getURL()}/account`
+    });
 
-        if (!customer) throw Error('Could not get customer');
-        const { url } = await stripe.billingPortal.sessions.create({
-            customer,
-            return_url: `${getURL()}/account`
-        });
-
-        return NextResponse.json({ url });
-    } catch (err: any) {
-        console.log(err);
-        new NextResponse('Internal Error', { status: 500 })
-    }
+    return NextResponse.json({ url });
+  } catch (err: any) {
+    console.log(err);
+    new NextResponse('Internal Error', { status: 500 })
+  }
 };
